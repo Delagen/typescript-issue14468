256b7cc0520ff7876b1f3d9d6d535b7d8b8522b6
Sergey Vohmyanin <svokhmyanin@colvir.com>
2017-03-08 00:36:34 +0300

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var tslib_1 = require("tslib");
+var A = (function () {
+    function A() {
+    }
+    return A;
+}());
+exports.A = A;
+var B = (function (_super) {
+    tslib_1.__extends(B, _super);
+    function B() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return B;
+}(A));
+exports.B = B;
